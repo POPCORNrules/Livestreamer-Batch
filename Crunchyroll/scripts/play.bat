441d5a3fe1b635a1b8bb128c958ab833e3c11d23
POPCORNrules <tarredondo@it-01.hsalaredo.org>
2016-01-25 13:29:24 -0600

    Remove SilmBoat Browser

--- Crunchyroll/scripts/play.bat
+++ Crunchyroll/scripts/play.bat
@@ -1,7 +1,7 @@
 @echo off
 color 0a
 title Playing %channel%
-livestreamer %login%%channel% %quality% -p %TP%\Programs\VLCPortable\app\vlc\VLC.exe
+livestreamer %login%%channel% %quality% -p %TP%\Programs\VLCPortable\app\vlc\VLC.exe --player-passthrough hls
 cls
 set "svdir=%save%\%channel% %mydate% %mytime%.mp4"
 For /f "tokens=1-2 delims=/ " %%a in ('time /t') do (set endtime=%%a%%b)
--- Updater.bat
+++ Updater.bat
@@ -14,21 +14,16 @@ rmdir /S /Q "%TP%\Programs\livestreamer"2>NUL >NUL
 rmdir /S /Q "%TP%\Programs\VLCPortable"2>NUL >NUL
 echo Downloading Please Wait...
 "%TP%\Programs\wget.exe" -q -b https://github.com/chrippa/livestreamer/releases/download/v%ver%/livestreamer-v%ver%-win32.zip -O "%TP%\Programs\Temp\livestreamer-v%ver%-win32.zip" --no-check-certificate>NUL
-"%TP%\Programs\wget.exe" -q -b "http://www.slimboat.com/release/sbt7z_x86.exe" -O "%TP%\Programs\Temp\sbt7z_x86.exe">NUL
 "%TP%\Programs\wget.exe" -q "http://sourceforge.net/projects/portableapps/files/VLC Media Player Portable/VLCPortable_%vlc%.paf.exe/download" -O "%TP%\Programs\Temp\VLCPortable_%vlc%.paf.exe">NUL
 mkdir "%TP%\Programs">NUL
 cls
 DEL /Q "%TP%\wget-log*" 2>NUL >NUL
 echo Updating Please Wait...
 "%TP%\Programs\Temp\VLCPortable_%vlc%.paf.exe" /DESTINATION="%TP%\Programs\"
-"%TP%\Programs\temp\sbt7z_x86.exe" -o"%TP%\Twitch" -y
 "%TP%\Programs\7za.exe" x "%TP%\Programs\Temp\livestreamer-v%ver%-win32.zip" -o"%TP%\Programs\Temp">NUL
 mkdir "%TP%\Programs\livestreamer" 2>NUL >NUL
 robocopy "%TP%\Programs\Temp\livestreamer-v%ver%" "%TP%\Programs\livestreamer" /E>NUL
 cls
-echo Please close the browser.
-%TP%\Twitch\SlimBoat\SlimBoat.exe
-copy /Y "%TP%\Programs\slimboat.ini" "%TP%\Twitch\SlimBoat\appdata"
 rmdir /S /Q "%TP%\Programs\Temp">NUL
 DEL /Q "%TP%\wget-log*" 2>NUL >NUL
 cls
